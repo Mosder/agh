834d37cf42a1b99f9899ee13ca104ed339150492
Mosder <xmosder12@gmail.com>
2025-03-31 02:48:44 +0200

    sysopy 4 done

--- sem4/sysopy/lab04/zad1.c
+++ sem4/sysopy/lab04/zad1.c
@@ -7,18 +7,18 @@
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Nieprawidłowa liczba argumentów\n");
-        return 0;
+        return 1;
     }
     int processes_count = atoi(argv[1]);
     if (processes_count <= 0) {
         printf("Liczba procesów musi być liczbą całkowitą większą od 0\n");
-        return 0;
+        return 1;
     }
     for (int i = 0; i < processes_count; i++) {
         pid_t pid = fork();
         // check if it's a child process
         if (pid == 0) {
-            printf("pid macierzysty - %d, pid własny - %d\n", getppid(), getpid());
+            printf("pid macierzysty - %d, pid własny - %d\n", (int) getppid(), (int) getpid());
             return 0;
         }
     }
--- sem4/sysopy/lab04/zad2.c
+++ sem4/sysopy/lab04/zad2.c
@@ -1,25 +1,46 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+
+int global = 0;
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        printf("Nieprawidłowa liczba argumentów\n");
-        return 0;
+        printf("Wrong amount of arguments\n");
+        return 1;
     }
+    // Print out program name
+    printf("Program name: %s\n", argv[0]);
+
+    int local = 0;
+
     pid_t pid = fork();
-    if (pid == 0) {
+    // Error handling for child creation
+    if (pid == -1) {
+        printf("ERROR: Child process could not be created");
+        return 2;
+    }
+    // Child process
+    else if (pid == 0) {
         printf("child process\n");
-        printf("child pid = %d, parent pid = %d\n", getpid(), getppid());
-        printf("child's local = %d, child's global = %d\n", 1, 1);
+        global++;
+        local++;
+        printf("child pid = %d, parent pid = %d\n", (int) getpid(), (int) getppid());
+        printf("child's local = %d, child's global = %d\n", local, global);
         return execl("/bin/ls", "ls", argv[1], NULL);
     }
+    // Parent process
     else {
         printf("parent process\n");
-        printf("parent pid = %d, child pid = %d\n", getpid(), pid);
-        printf("Child exit code: %d\n", 1);
-        printf("Parent's local = %d, parent's global = %d\n", 1, 1);
-        return 0;
+        printf("parent pid = %d, child pid = %d\n", (int) getpid(), (int) pid);
+        int child_exit_code;
+        // Wait for child process to finish and save the exit code to a variable
+        wait(&child_exit_code);
+        // Print out converted exit status
+        printf("Child exit code: %d\n", WEXITSTATUS(child_exit_code));
+        printf("Parent's local = %d, parent's global = %d\n", local, global);
+        return WEXITSTATUS(child_exit_code);
     }
     return 0;
 }
