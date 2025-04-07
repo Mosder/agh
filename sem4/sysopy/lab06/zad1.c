4326fee3b6ebd69ba935e4796d37b8a1c0a5f3dc
Mosder <xmosder12@gmail.com>
2025-04-07 16:07:39 +0200

    sysopy 6 - zad1 done

--- sem4/sysopy/lab06/zad1.c
+++ sem4/sysopy/lab06/zad1.c
@@ -27,15 +27,15 @@ double calculate_integral(int process_count, double dx, double *t) {
             snprintf(sub_result_buffer, BUFFER_LIMIT, "%f", sub_result);
             write(fd[process_count][1], sub_result_buffer, sizeof(char) * BUFFER_LIMIT);
             close(fd[process_count][1]);
-            return 0;
+            exit(0);
         }
-        else {
-            close(fd[process_count][1]);
-            char sub_result[BUFFER_LIMIT];
-            read(fd[process_count][0], sub_result, sizeof(char) * BUFFER_LIMIT);
-            close(fd[process_count][0]);
-            result += strtod(sub_result, NULL);
-        }
+        close(fd[process_count][1]);
+    }
+    char sub_result[BUFFER_LIMIT];
+    for (int proc = 0; proc < process_count; proc++) {
+        read(fd[process_count][0], sub_result, sizeof(char) * BUFFER_LIMIT);
+        close(fd[process_count][0]);
+        result += strtod(sub_result, NULL);
     }
     clock_gettime(CLOCK_MONOTONIC, &t1);
     *t = t1.tv_sec - t0.tv_sec + 1.0e-9*t1.tv_nsec - 1.0e-9*t0.tv_nsec;
@@ -58,7 +58,7 @@ int main(int argc, char *argv[]) {
     double result, time_elapsed;
     for (int i = 1; i <= n; i++) {
         result = calculate_integral(i, dx, &time_elapsed);
-        printf("result: %f, elapsed time: %fs\n", result, time_elapsed);
+        printf("processes: %d, result: %f, elapsed time: %fs\n", i, result, time_elapsed);
     }
     return 0;
 }
