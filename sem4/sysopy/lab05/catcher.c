0dff6fb2cec08d86af8d1d88e13e359914882c8f
Mosder <xmosder12@gmail.com>
2025-04-07 09:55:03 +0200

    sysopy 5 done

--- sem4/sysopy/lab05/catcher.c
+++ sem4/sysopy/lab05/catcher.c
@@ -1,16 +1,73 @@
+#include <unistd.h>
+#include <stdio.h>
 #include <signal.h>
 
-void handler(int signum, siginfo_t *si, void *p3) {
-    printf("Signal handle: %d %d, war: %d\n", si->si_pid, si->si_uid, si->si_value);
+#define PRINT_SIG_COUNT 1
+#define PRINT_EVERY_SEC 2
+#define IGNORE_CTRL_C 3
+#define PRINT_CTRL_C 4
+#define KILL 5
+
+int received_signals = 0;
+int number = 0;
+
+void handle_ctrl_c() {
+    printf("Wciśnięto CTRL+C\n");
+    return;
+}
+
+void handler(int sign, siginfo_t *info, void *p3) {
+    received_signals++;
+    kill(info->si_pid, SIGUSR1);
+    int mode = info->si_value.sival_int;
+    if (mode != PRINT_EVERY_SEC) number = 0;
+
+    switch(mode) {
+        case PRINT_SIG_COUNT: {
+            printf("Całkowita liczba otrzymanych sygnałów: %d\n", received_signals);
+            break;
+        }
+        case PRINT_EVERY_SEC: {
+            sigset_t newmask, oldmask, set;
+            sigemptyset(&newmask);
+            sigaddset(&newmask, SIGUSR1);
+            sigprocmask(SIG_BLOCK, &newmask, &oldmask);
+            while(1) {
+                printf("Number: %d\n", number++);
+                sleep(1);
+                sigpending(&set);
+                if (sigismember(&set, SIGUSR1)) {
+                    sigprocmask(SIG_UNBLOCK, &newmask, &oldmask);
+                    break;
+                }
+            }
+            break;
+        }
+        case IGNORE_CTRL_C: {
+            signal(SIGINT, SIG_IGN);
+            break;
+        }
+        case PRINT_CTRL_C: {
+            signal(SIGINT, handle_ctrl_c);
+            break;
+        }
+        case KILL: {
+            raise(SIGKILL);
+            break;
+        }
+    }
+    return;
 }
 
 int main(void) {
-    sigset_t set;
-    struct sigaction act;
     printf("pid: %d\n", getpid());
 
-    act.sa_sigaction = handler;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = SA_SIGINFO;
-    sigaction(SIGUSR1, &act, NULL);
+    struct sigaction action;
+    action.sa_sigaction = handler;
+    action.sa_flags = SA_SIGINFO;
+    sigaction(SIGUSR1, &action, NULL);
+
+    while(1) pause();
+
+    return 0;
 }
--- sem4/sysopy/lab05/sender.c
+++ sem4/sysopy/lab05/sender.c
@@ -1,10 +1,33 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <signal.h>
 
-int main(void) {
-    union sigval war;
-    int pid = atoi(tab[1]);
+void handle_response() {
+    printf("Potwierdzono odebranie sygnału\n");
+    return;
+}
 
-    war.sival_int = 123;
-    sigqueue(pid, SIGUSR1, war);
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Nieprawidłowa liczba argumentów\n");
+        return 1;
+    }
+
+    int catcher_pid = atoi(argv[1]);
+    int mode = atoi(argv[2]);
+
+    if (catcher_pid == 0 || mode < 1 || mode > 5) {
+        printf("Otrzymano nieprawidłowe argumenty\n");
+        return 1;
+    }
+
+    signal(SIGUSR1, handle_response);
+
+    union sigval message;
+    message.sival_int = mode;
+    sigqueue(catcher_pid, SIGUSR1, message);
+
+    pause();
     return 0;
 }
