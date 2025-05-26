5c93aacf96515ed7d96803290dfec81c02f77257
mborowiec mborowiec <mborowiec@net.kis.agh.edu.pl@r208-c13.net.kis.agh.edu.pl>
2025-05-26 14:07:56 +0200

    sysopy 11 example

---
+++ sem4/sysopy/lab11/ex_client.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main(void) {
+    int status, gniazdo;
+    struct sockaddr_in ser, cli;
+    char buf[200];
+
+    gniazdo = socket(AF_INET, SOCK_STREAM, 0);
+    if (gniazdo == -1) {printf("blad socket\n"); return 0;}
+
+    memset(&ser, 0, sizeof(ser));
+    ser.sin_family = AF_INET;
+    ser.sin_port = htons(9000);
+    ser.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    status = connect(gniazdo, (struct sockaddr*)&ser, sizeof ser);
+    if (status < 0) {printf("blad connect\n"); return 0;}
+
+    printf("Podaj tekst:");
+    fgets(buf, sizeof buf, stdin);
+    status = write(gniazdo, buf, strlen(buf));
+    status = read(gniazdo, buf, (sizeof buf)-1);
+    buf[status] = '\0';
+    printf("Otrzymalem: %s\n", buf);
+    
+    close(gniazdo);
+    return 0;
+}
